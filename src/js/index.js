ad8fe4c9c5c1b7d7b9ba173be77dc224fbd2939d
Cangre1 <suareznicolasm@gmail.com>
2023-08-22 10:28:39 -0300

    update Nico

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,50 @@
+//FUNCIÓN HERO DESKTOP//
+// Obtener todos los elementos con la clase "card-img"
+let cardImgs = document.querySelectorAll(".card-img");
+
+// Función para mostrar elementos
+function showElements(event) {
+  let textElement = event.currentTarget.querySelector(".text-card");
+  let arrowElement = event.currentTarget.querySelector(".arrow-card");
+  textElement.classList.remove("hidden");
+  arrowElement.classList.remove("hidden");
+  textElement.classList.add("transition");
+  textElement.classList.add("duration-300");
+  textElement.classList.add("ease-in-out");
+}
+
+// Función para ocultar elementos
+function hideElements(event) {
+  let textElement = event.currentTarget.querySelector(".text-card");
+  let arrowElement = event.currentTarget.querySelector(".arrow-card");
+  textElement.classList.add("hidden");
+  arrowElement.classList.add("hidden");
+}
+
+// Agregar eventos a todos los elementos con la clase "card-img"
+cardImgs.forEach((cardImg) => {
+  // Evento mouseover: Mostrar elementos al pasar el cursor
+  cardImg.addEventListener("mouseover", showElements);
+  // Evento mouseout: Ocultar elementos al quitar el cursor
+  cardImg.addEventListener("mouseout", hideElements);
+
+  // Evento click: Alternar elementos al hacer clic
+  cardImg.addEventListener("click", (event) => {
+    event.preventDefault(); // Evitar la acción predeterminada del clic (por ejemplo, seguir un enlace)
+    let textElement = event.currentTarget.querySelector(".text-card");
+    let arrowElement = event.currentTarget.querySelector(".arrow-card");
+
+    if (textElement.classList.contains("hidden")) {
+      textElement.classList.remove("hidden");
+      arrowElement.classList.remove("hidden");
+    } else {
+      textElement.classList.add("hidden");
+      arrowElement.classList.add("hidden");
+    }
+  });
+});
+
+//FUNCIÓN DE SECCIONES DE INFORMACION//
 function toggleSliderActive(link, event) {
   event.preventDefault(); // Detener el comportamiento predeterminado del enlace
 
